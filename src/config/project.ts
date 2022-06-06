395dba77c775f94dc1ad43c5c48eb2b209f2925f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-06 10:33:43 -0700

    firebase project

--- src/config/project.ts
+++ src/config/project.ts
@@ -1,11 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAd-pX35BsCvIBDXstf_LmQaz82PNFGcMI",
-  authDomain: "fir-vue-startup-kit.firebaseapp.com",
-  databaseURL: "https://fir-vue-startup-kit.firebaseio.com",
-  projectId: "fir-vue-startup-kit",
-  storageBucket: "fir-vue-startup-kit.appspot.com",
-  messagingSenderId: "225404938985",
-  appId: "1:225404938985:web:a2cea9fab1722fce",
-  messageKey:
-    "BGwIKr3yZdXmg4Pl-BpIQX_ETtHYl0RqvAJFoc-hYKFyaoU3wZXarqto5DSq1cemFuARMjFvJ02B83T5ga42MKo",
+  apiKey: "AIzaSyDtKirQnr6V5nkanGo_W_OnacZs6otjKqM",
+  authDomain: "nounsville.firebaseapp.com",
+  projectId: "nounsville",
+  storageBucket: "nounsville.appspot.com",
+  messagingSenderId: "662811584578",
+  appId: "1:662811584578:web:422dea65c91b17a2a4a5db",
+  measurementId: "G-69XJCDQGYP"
 };
