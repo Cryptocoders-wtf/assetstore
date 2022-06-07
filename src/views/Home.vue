18d54677c29685eb6366a16ce94cc6b6dee8a485
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-07 12:42:02 -0700

    Hello World

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <div class="flex justify-center items-center space-x-8">
       <!-- Use Tailwind CSS h-40 (=10rem=160px) instead of .logo. -->
+      <p>Hello World</p>
     </div>
   </div>
 </template>
