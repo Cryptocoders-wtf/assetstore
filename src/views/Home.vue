dfc2591b220cf9e3945c8e033b51cf75aecdcb83
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-07 14:00:55 -0700

    tokenBalance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="flex justify-center items-center space-x-8">
-    <div v-if="isValidNetwork">
-      <p>Hello World</p>
+    <div v-if="tokenGate == 'noAccount'">
+      <p>{{ tokenGate }}</p>
+    </div>
+    <div v-else-if="tokenGate == 'invalidNetwork'">
+      <p>{{ tokenGate }}</p>
+      <button @click="switchToValidNetwork" class="underline">Switch Network</button>
     </div>
     <div v-else>
-      <p>Invalid Network</p>
-      <button @click="switchToValidNetwork" class="underline">Switch Network</button>
+      <div v-if="tokenBalance > 0">
+        <p>Hello a member</p>
+      </div>
+      <div v-else>
+        <p>You need to have a Nounsville token to play with this app</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
+import { defineComponent, computed, ref } from "vue";
 import { useStore } from "vuex";
 import { ethers } from "ethers";
 import { ChainIds, switchNetwork } from "../utils/MetaMask";
+
+const NounsVille = {
+  wabi: require("../abis/NounsvilleToken.json"), // wrapped abi
+  address: "0x163B3906884df904EFF51bf21E7Ee3D3f87098D3"
+};
 
 export default defineComponent({
   name: "HomePage",
@@ -23,16 +36,36 @@ export default defineComponent({
   setup() {
     const expectedNetwork = ChainIds.RinkebyTestNet;
     const store = useStore();
-    const isValidNetwork = computed(()=>{
-      return store.state.chainId == expectedNetwork;
-    });    
+    const tokenBalance = ref(0);
+    const account = computed(() => {
+      return store.state.account;
+    });
+    const fetchBalance = async () => {
+      const provider = new ethers.providers.Web3Provider(store.state.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(NounsVille.address, NounsVille.wabi.abi, signer);
+      const count = await contract.functions.balanceOf(account.value);
+      console.log("**** count", count[0].toNumber());
+      tokenBalance.value = count[0].toNumber();
+    };
+    const tokenGate = computed(()=>{
+      if (!account.value) {
+        return "noAccount"
+      }
+      if (store.state.chainId != expectedNetwork) {
+        return "invalidNetwork"
+      }
+      fetchBalance();
+      return "valid";      
+    });
     const switchToValidNetwork = async () => {
       console.log(expectedNetwork);
       await switchNetwork(expectedNetwork);
     }
 
     return {
-      isValidNetwork,
+      tokenGate,
+      tokenBalance,
       switchToValidNetwork
     }
   }
