c9134280ab399f42d67d953cf9b7fba3197095dd
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-03 04:15:06 +0900

    fixed localhost

--- src/views/LocalMint.vue
+++ src/views/LocalMint.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-xl mx-auto text-left p-2">
     <mint-view
-      network="rinkeby" 
+      network="localhost" 
       :expectedNetwork="expectedNetwork"
       storeAddress="0xB0f3A4aE1fDC1068f9364c5d7b1E42678B66D941"
       tokenAddress="0xd5660525C2378294bfe3b8197f714CcBFD6654bb"
