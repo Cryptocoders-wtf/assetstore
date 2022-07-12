aeca80353899cb01eb594c7e54dd709a0e7f012a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-12 13:43:45 +0900

    use generated

---
+++ generated/addresses_localhost.ts
@@ -0,0 +1,4 @@
+export const addresses = {
+  storeAddress:"0x9DBb24B10502aD166c198Dbeb5AB54d2d13AfcFd",
+  tokenAddress:"0xF8b299F87EBb62E0b625eAF440B73Cc6b7717dbd"
+}
---
+++ generated/addresses_rinkeby.ts
@@ -0,0 +1,4 @@
+export const addresses = {
+  storeAddress:"0x7010bDCEEE3438a3a1a7EEd184AA9F37cE7f4586",
+  tokenAddress:"0x9EA5233a18380eC04BDC4606b8475109Ad97a8A9"
+}
