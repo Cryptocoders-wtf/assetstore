0d26de4cf00ed0fb1dfe4e1568508ba8f276bf3d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-12 21:24:29 +0900

    Category Name

--- src/resources/materials.ts
+++ src/resources/materials.ts
@@ -4,7 +4,7 @@ import { assets } from "../../generated/communication";
 
 export const actions = {
   group: "Material Icons (Apache 2.0)",
-  category: "Audio and Video",
+  category: "Communication",
   width: 24, height: 24,
   assets: assets
 };
