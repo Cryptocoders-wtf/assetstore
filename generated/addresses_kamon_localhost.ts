b4f6fd51609c82f8aaf29e2a2dc4e26a316c4c8a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-17 08:50:14 +0900

    addresses_kamon

---
+++ generated/addresses_kamon_localhost.ts
@@ -0,0 +1,4 @@
+export const kamon_addresses = {
+  decoderAddress:"0xf18774574148852771c2631d7d06E2A6c8b44fCA",
+  kamonAddress:"0x20BBE62B175134D21b10C157498b663F048672bA"
+}
--- generated/addresses_localhost.ts
+++ generated/addresses_localhost.ts
@@ -1,4 +1,4 @@
 export const addresses = {
-  storeAddress:"0x9DBb24B10502aD166c198Dbeb5AB54d2d13AfcFd",
-  tokenAddress:"0xF8b299F87EBb62E0b625eAF440B73Cc6b7717dbd"
+  storeAddress:"0x88D1aF96098a928eE278f162c1a84f339652f95b",
+  tokenAddress:"0x7Ce73F8f636C6bD3357A0A8a59e0ab6462C955B0"
 }
