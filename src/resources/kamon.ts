7a39d1e8ec0adaab55d5d29d67f3e0a7040cca57
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-17 09:46:52 +0900

    loadedAssets

---
+++ src/resources/kamon.ts
@@ -0,0 +1,12 @@
+import { loadAssets } from "../utils/createAsset";
+
+import { assets } from "../../generated/hardware";
+
+export const actions = {
+  group: "Material Icons (Apache 2.0)",
+  category: "Hardware",
+  width: 24, height: 24,
+  assets: assets
+};
+
+export const loadedAssets = loadAssets(actions);
