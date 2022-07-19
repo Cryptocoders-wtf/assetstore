afd9a7efc1acc25110bd5264becabb02fea4439c
isamu <isamu@to-kyo.to>
2022-07-19 11:52:17 +0900

    add more types

--- src/models/asset.ts
+++ src/models/asset.ts
@@ -5,3 +5,23 @@ export interface AssetData {
   index: number;
   svg: string;
 }
+
+
+// for import data
+export interface OriginalAssetData {
+  name: string;
+  group?: string;
+  category?: string;
+  width?: number;
+  height?: number;
+  bodies?: string[];
+  parts?: { body: string; }[]
+  body?: string;
+}
+export interface OriginalAssetDataSet {
+  group?: string;
+  category?: string;
+  width?: number;
+  height: number;
+  assets: OriginalAssetData[];
+}
