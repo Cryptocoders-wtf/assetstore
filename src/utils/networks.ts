a03f565cdc6d8caaf2f9e57b42c0ceb9fdb61441
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-20 16:02:00 -0700

    transferred

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -3,6 +3,7 @@ import { ChainIds } from "../utils/MetaMask";
 import { addresses as mainnet } from "../../generated/addresses_mainnet";
 import { addresses as localhost } from "../../../contract/cache/addresses_localhost";
 import { addresses as rinkeby } from "../../../contract/cache/addresses_rinkeby";
+import { kamon_addresses as kamon_mainnet } from "../../generated/addresses_kamon_mainnet";
 import { kamon_addresses as kamon_localhost } from "../../../contract/cache/addresses_kamon_localhost";
 import { kamon_addresses as kamon_rinkeby } from "../../../contract/cache/addresses_kamon_rinkeby";
 
@@ -32,7 +33,8 @@ export const getAddresses = (network: string) => {
       EtherscanBase: "https://etherscan.io/address",
       chainId: ChainIds.Mainnet,
       storeAddress: mainnet.storeAddress,
-      tokenAddress: mainnet.tokenAddress
+      tokenAddress: mainnet.tokenAddress,
+      kamonAddress: kamon_mainnet.kamonAddress
     }
   }
   console.error("**** unexpected");
