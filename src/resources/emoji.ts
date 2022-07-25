2e1ea0ce8f45162f5940bc614551af637b244dad
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-25 14:59:08 -0700

    OpenMoji

--- src/resources/emoji.ts
+++ src/resources/emoji.ts
@@ -3,7 +3,7 @@ import { loadAssets } from "../utils/createAsset";
 import { assets } from "../../generated/emoji_assets";
 
 export const actions = {
-  group: "Open Emoji (CC-BY)",
+  group: "OpenMoji (CC BY-SA 4.0)",
   category: "Flags",
   width: 72,
   height: 72,
