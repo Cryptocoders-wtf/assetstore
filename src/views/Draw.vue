1ea69be644beddf911fa4d76191c7e0f8ae4c1f8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-26 13:49:11 -0700

    border for cursor

--- src/views/Draw.vue
+++ src/views/Draw.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="max-w-xl mx-auto text-left p-2">
     <div style="width:512; height:512px" class="bg-slate-300">
+      <div v-for="(cursor, index) in cursors" :key="index"
+        style="width:40px; height:40px; position:relative" 
+        class="border-2 border-solid border-blue-700">
+      </div>
     </div>
   </div>
 </template>
