cfa297afd42d099140104c8819bf2aafe82d95bb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-07-26 13:41:46 -0700

    cursors

--- src/views/Draw.vue
+++ src/views/Draw.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="max-w-xl mx-auto text-left p-2">
-    <p>Hello</p>
+    <div style="width:512; height:512px" class="bg-slate-300">
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+
+interface Point {
+  x: number;
+  y: number;
+  c: boolean;
+}
 
 export default defineComponent({
   name: "HomePage",
   components: {
   },
   setup() {
+    const cursors = ref<Point[]>([]);
+    cursors.value = [
+      { x:128, y:128, c:false },
+      { x:128, y:384, c:false },
+      { x:384, y:384, c:false },
+      { x:384, y:128, c:false },
+    ]
     return {
+      cursors
     };
   },
 });
