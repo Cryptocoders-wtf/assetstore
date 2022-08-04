3f05908a05f5ad863976202d30985873feea7fa0
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-04 08:31:01 -0700

    display token, onSelect

--- src/components/TokenPicker.vue
+++ src/components/TokenPicker.vue
@@ -5,13 +5,21 @@
     </button>
     <div v-if="showTokens" 
       style="width:400px;height:400px; left:-350px"
-      class="absolute border-2 border-solid border-blue-700 bg-slate-300">
-      Hello World
+      class="absolute border-2 border-solid border-blue-700 bg-slate-100">
+      <span v-for="token in tokens" :key="token.tokenId">
+        <img
+          @click="onSelect(token)"
+          :src="token.image"
+          class="mr-1 mb-1 inline-block w-14 rounded-xl"
+        />
+      </span>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { Token } from "@/models/token";
+
 export default defineComponent({
   props: ["tokens"],
   setup(props) {
@@ -20,9 +28,13 @@ export default defineComponent({
       console.log("onOpen");
       showTokens.value = !showTokens.value;
     }
+    const onSelect = (token:Token) => {
+      console.log("onSelect", token.tokenId);
+    }
     return {
       onOpen,
-      showTokens
+      showTokens,
+      onSelect
     };
   }
 });
