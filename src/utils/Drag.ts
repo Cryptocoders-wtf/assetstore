19d81ba1f5b781fbdf1b0c39b8bad21692d35c9a
isamu <isamu@to-kyo.to>
2022-08-06 14:17:05 +0900

    separate drag

---
+++ src/utils/Drag.ts
@@ -0,0 +1,188 @@
+import { ref, Ref, ComputedRef } from "vue";
+
+import {
+  getPageX,
+  getPageY,
+  getOffsetX,
+  getOffsetY,
+  canvasParams,
+  Pos,
+  UIPos,
+  Tools,
+  RotationInfo,
+} from "@/utils/canvasUtil";
+import { Point } from "@/models/point";
+
+const { curw, curh } = canvasParams;
+
+export const useDrag = (
+  pointIndex: Ref<number>,
+  moveToolPos: ComputedRef<UIPos>,
+  cursors: Ref<Point[]>,
+  recordState: () => void
+) => {
+  const pivotPos = ref<Pos>({ x: 0, y: 0 });
+  const offsetX = ref<number>(0);
+  const offsetY = ref<number>(0);
+  const startPoint = ref<Pos>({ x: 0, y: 0 });
+  const initialCursors = ref<Point[]>([]);
+  const grid = ref<number>(0);
+
+  const currentTool = ref<Tools>(0);
+
+  const toggleGrid = () => {
+    grid.value = (grid.value + 8) % 40;
+  };
+
+  const dragToolHandleStart = (evt: DragEvent | TouchEvent, tool: Tools) => {
+    currentTool.value = tool;
+    offsetX.value = curw / 2;
+    offsetY.value = curh / 2;
+    startPoint.value.x = getPageX(evt);
+    startPoint.value.y = getPageY(evt);
+    pivotPos.value = moveToolPos.value;
+    initialCursors.value = cursors.value;
+    recordState();
+  };
+  const dragLayerImgStart = (evt: MouseEvent | TouchEvent) => {
+    currentTool.value = Tools.MOVE;
+    offsetX.value = curw / 2;
+    offsetY.value = curh / 2;
+    startPoint.value.x = getPageX(evt);
+    startPoint.value.y = getPageY(evt);
+    initialCursors.value = cursors.value;
+    recordState();
+  };
+  const dragStart = (evt: DragEvent | TouchEvent, index: number) => {
+    currentTool.value = Tools.CURSOR;
+    offsetX.value = getOffsetX(evt);
+    offsetY.value = getOffsetY(evt);
+    pointIndex.value = index;
+    recordState();
+  };
+  const dragOver = (evt: DragEvent | TouchEvent) => {
+    const { offx, offy } = canvasParams;
+    const g = grid.value;
+    const gridder = (pos: Pos): Pos => {
+      const f = (n: number) => (g == 0 ? n : Math.round(n / g) * g);
+      return {
+        x: f(pos.x),
+        y: f(pos.y),
+      };
+    };
+    const limiter = (pos: Pos): Pos => {
+      const { canw, canh } = canvasParams;
+      const f = (can: number, n: number, offset: number, cur: number) =>
+        Math.max(0, Math.min(can - g - 1, n - offset + cur / 2));
+      return {
+        x: f(canw, pos.x, offsetX.value, curw),
+        y: f(canh, pos.y, offsetY.value, curh),
+      };
+    };
+    const magnification =
+      currentTool.value === Tools.ZOOM &&
+      Math.abs(pivotPos.value.y + offy - startPoint.value.y) !== 0
+        ? Math.abs(pivotPos.value.y + offy - getPageY(evt)) /
+          Math.abs(pivotPos.value.y + offy - startPoint.value.y)
+        : 0;
+    const rad =
+      currentTool.value === Tools.ROTATE
+        ? pivotPos.value.x + offx - startPoint.value.x > 1
+          ? Math.atan2(
+              pivotPos.value.y + offy - getPageY(evt),
+              pivotPos.value.x + offx - getPageX(evt)
+            )
+          : (Math.atan2(
+              pivotPos.value.y + offy - getPageY(evt),
+              pivotPos.value.x + offx - getPageX(evt)
+            ) +
+              Math.PI) %
+            (2 * Math.PI)
+        : 0;
+    const RotationInfo: RotationInfo =
+      currentTool.value === Tools.ROTATE
+        ? {
+            radian: rad,
+            sin: Math.sin(rad),
+            cos: Math.cos(rad),
+          }
+        : { radian: 0, sin: 0, cos: 0 };
+    cursors.value = cursors.value.map((cursor, index) => {
+      switch (currentTool.value) {
+        case Tools.ZOOM:
+          return {
+            ...gridder(
+              limiter({
+                x:
+                  initialCursors.value[index].x * magnification +
+                  (pivotPos.value.x - pivotPos.value.x * magnification),
+                y:
+                  initialCursors.value[index].y * magnification +
+                  (pivotPos.value.y - pivotPos.value.y * magnification),
+              })
+            ),
+            c: cursor.c,
+          };
+        case Tools.ROTATE:
+          return {
+            ...gridder(
+              limiter({
+                x:
+                  (initialCursors.value[index].x - pivotPos.value.x) *
+                    RotationInfo.cos -
+                  (initialCursors.value[index].y - pivotPos.value.y) *
+                    RotationInfo.sin +
+                  pivotPos.value.x,
+                y:
+                  (initialCursors.value[index].x - pivotPos.value.x) *
+                    RotationInfo.sin +
+                  (initialCursors.value[index].y - pivotPos.value.y) *
+                    RotationInfo.cos +
+                  pivotPos.value.y,
+              })
+            ),
+            c: cursor.c,
+          };
+        case Tools.MOVE:
+          return {
+            ...gridder(
+              limiter({
+                x:
+                  initialCursors.value[index].x -
+                  (startPoint.value.x - getPageX(evt)),
+                y:
+                  initialCursors.value[index].y -
+                  (startPoint.value.y - getPageY(evt)),
+              })
+            ),
+            c: cursor.c,
+          };
+        case Tools.CURSOR:
+        default:
+          if (index == pointIndex.value) {
+            return {
+              ...gridder(
+                limiter({
+                  x: getPageX(evt) - offx - 3,
+                  y: getPageY(evt) - offy - 3,
+                })
+              ),
+              c: cursor.c,
+            };
+          }
+      }
+      return cursor;
+    });
+    evt.preventDefault();
+  };
+
+  return {
+    dragLayerImgStart,
+    dragToolHandleStart,
+    dragStart,
+    dragOver,
+    currentTool,
+    toggleGrid,
+    grid,
+  };
+};
