71e66dd406430ed187b42151b9d95f7b1d4aa924
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-06 20:25:11 -0700

    ishi kamon

--- src/resources/kamon.ts
+++ src/resources/kamon.ts
@@ -4,7 +4,7 @@ import { assets } from "../../generated/kamon_assets";
 
 export const actions = {
   group: "Hakko Daiodo (CC-BY equivalent)",
-  category: "Igeta",
+  category: "Ishi",
   width: 24,
   height: 24,
   assets: assets,
