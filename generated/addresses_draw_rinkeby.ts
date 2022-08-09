1f473d235f5cf76e8cbdb2bd2aa9a3d3fd23ab5a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-09 04:35:49 -0700

    IAssetStoreEx hack

--- generated/addresses_draw_rinkeby.ts
+++ generated/addresses_draw_rinkeby.ts
@@ -1,4 +1,4 @@
 export const token_addresses = {
-  customTokenAddress:"0x0E703B0E5B77B9253A5a470BB9e02AEEd19f5A49",
-  composerAddress:"0x1fF095Ed7b166d1753b949D919Bdf7311Ef72f9d"
+  customTokenAddress:"0xA9aa33b2593f52a4E49afA36D19f207cC4ff8088",
+  composerAddress:"0x38EBf94d47cAD249a7cdD0B93CC8b8248A913c13"
 }
