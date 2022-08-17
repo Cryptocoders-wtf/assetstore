a2096510737315d9a04643367c408681ef8b6d5d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-17 14:23:39 -0700

    Kamons/Kai

--- src/resources/kamon.ts
+++ src/resources/kamon.ts
@@ -4,7 +4,7 @@ import { assets } from "../../generated/kamon_assets";
 
 export const actions = {
   group: "Hakko Daiodo (CC-BY equivalent)",
-  category: "Ougi",
+  category: "Kai",
   width: 24,
   height: 24,
   assets: assets,
