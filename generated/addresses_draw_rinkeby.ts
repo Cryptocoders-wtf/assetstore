4fdf64d11cfa0cd368d5e59f5edfc96307a409b9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-08-29 10:15:02 -0700

    updated contract address

--- generated/addresses_draw_rinkeby.ts
+++ generated/addresses_draw_rinkeby.ts
@@ -1,4 +1,4 @@
 export const token_addresses = {
-  customTokenAddress:"0x9b52461c6d29F6Ba45c06A281DAC1Dd73478B89d",
-  composerAddress:"0x914851674e1ddeb9720e4A7c6Eb2B0EEbEd7D996"
+  customTokenAddress:"0x171e115A40eC0FE00bD5771461dD1E4f9B3a6aD9",
+  composerAddress:"0xB70710b31e85642A4cD0415C35f9Ea40f4C55D4E"
 }
