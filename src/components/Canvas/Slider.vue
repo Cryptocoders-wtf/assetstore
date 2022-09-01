8d131be88b4278fcb13173a2f0784bf0fa102133
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-01 13:55:30 -0700

    text input

--- src/components/Canvas/Slider.vue
+++ src/components/Canvas/Slider.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex bg-slate-400 mb-1" :style="`width:${sliderWidth}px; height:24px`" @mousedown="mousedown">
+  <div class="flex bg-slate-400 mb-1" :style="`width:${sliderWidth}px; height:28px`" @mousedown="mousedown">
     <div v-if="value">
-      <span class="absolute">{{value}}</span>
-      <div class="bg-blue-300" :style="`width:${sliderWidth*value}px; height:24px`" />
+      <input class="absolute text-sm" type="value" :value="value" :style="`width:${sliderWidth}px; height:16px`"> 
+      <div class="bg-blue-300" :style="`width:${sliderWidth*value}px; height:28px`" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
 
 export default defineComponent({
   props: ["value"],
@@ -17,10 +17,11 @@ export default defineComponent({
     const sliderWidth = 80;
     const mousedown = (evt:MouseEvent) => {
       context.emit("updateValue", evt.offsetX/sliderWidth);
+      evt.preventDefault();
     };
     return {
       sliderWidth,
-      mousedown
+      mousedown,
     };
   }
 });
