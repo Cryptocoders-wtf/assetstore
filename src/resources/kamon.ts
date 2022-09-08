c2079089b80be989b6c13b7eb5b1976ddf0406b7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-08 15:31:16 -0700

    Kuruma

--- src/resources/kamon.ts
+++ src/resources/kamon.ts
@@ -4,7 +4,7 @@ import { assets } from "../../generated/kamon_assets";
 
 export const actions = {
   group: "Hakko Daiodo (CC-BY equivalent)",
-  category: "Kuwagata",
+  category: "Kuruma",
   width: 24,
   height: 24,
   assets: assets,
