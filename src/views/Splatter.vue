b026d83fdd3cca27576cbf3d30ecc9eb819824f5
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-13 06:00:36 -0700

    dot at the end

--- src/views/Splatter.vue
+++ src/views/Splatter.vue
@@ -15,17 +15,47 @@ export default defineComponent({
     }; 
     const points:Point[] = [];
     const [cx, cy] = [512, 512];
-    const r0 = 200;
+    const r0 = 202;
     var alt = false;
-    for (var i = 0; i < Math.PI * 2; i += foo(0.15, 1.8)) {
-      const r = alt ? foo(1, 0.5) : foo(0.5, 0.4);
+    var speed = Math.PI / 30;
+    for (var i = 0; i < Math.PI * 2; i += foo(speed, 2)) {
+      const r = alt ? foo(0.8, 1.2) : foo(0.5, 0.4);
       alt = !alt;
-      points.push({
+      if (r > 1) {
+        const arc = 0.1;
+        points.push({
+          x: cx + r0 * r * (1-arc) * Math.cos(i - speed * 0.03),
+          y: cy + r0 * r * (1-arc) * Math.sin(i - speed * 0.03),
+          c: false,
+          r: 0.588
+        });
+        points.push({
+          x: cx + r0 * r * Math.cos(i - speed * 0.2),
+          y: cy + r0 * r * Math.sin(i - speed * 0.2),
+          c: false,
+          r: 0.588
+        });
+        points.push({
+          x: cx + r0 * r * Math.cos(i + speed * 0.2),
+          y: cy + r0 * r * Math.sin(i + speed * 0.2),
+          c: false,
+          r: 0.588
+        });
+        points.push({
+          x: cx + r0 * r * (1-arc)  * Math.cos(i + speed * 0.03),
+          y: cy + r0 * r * (1-arc)  * Math.sin(i + speed * 0.03),
+          c: false,
+          r: 0.588
+        });
+      } else {
+        points.push({
         x: cx + r0 * r * Math.cos(i),
         y: cy + r0 * r * Math.sin(i),
-        c: (r > 1.2),
+        c: false,
         r: alt ? foo(1.0, 0.3) : 0.588
       })
+
+      }
     }
     const path = pathFromPoints(points);
     const image = svgImageFromPath(path, "red");
