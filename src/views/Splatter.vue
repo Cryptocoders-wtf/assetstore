ec489d7a764c2f860210f26c49fcb4dcd1d8fa7c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-13 03:52:06 -0700

    more randomness

--- src/views/Splatter.vue
+++ src/views/Splatter.vue
@@ -16,14 +16,14 @@ export default defineComponent({
     const points:Point[] = [];
     const [cx, cy] = [512, 512];
     const r0 = 200;
-    var alt = true;
-    for (var i = 0; i < Math.PI * 2; i += foo(0.1, 0.3)) {
-      const r = alt ? foo(1, 0.8) : foo(0.5, 0.4);
+    var alt = false;
+    for (var i = 0; i < Math.PI * 2; i += foo(0.15, 1.8)) {
+      const r = alt ? foo(1, 0.5) : foo(0.5, 0.4);
       alt = !alt;
       points.push({
         x: cx + r0 * r * Math.cos(i),
         y: cy + r0 * r * Math.sin(i),
-        c: (r > 1.4),
+        c: (r > 1.2),
         r: alt ? foo(1.0, 0.3) : 0.588
       })
     }
