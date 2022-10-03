ebd6fbef49a8f949f746259d3c0a5cfc590ced77
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-02 19:40:46 -0700

    kamon/jigami

--- src/resources/kamon.ts
+++ src/resources/kamon.ts
@@ -4,7 +4,7 @@ import { assets } from "../../generated/kamon_assets";
 
 export const actions = {
   group: "Hakko Daiodo (CC-BY equivalent)",
-  category: "Sasa",
+  category: "Jigami",
   width: 24,
   height: 24,
   assets: assets,
